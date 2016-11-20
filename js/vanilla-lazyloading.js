4b6bf93708de31652a7992c35f89cc1763e265e1
Kevin <kevin@rocher-dev.com>
2016-11-20 23:02:17 +0100

    v 0.6.0
    * Action children : load child images when interacting with items.

--- js/vanilla-lazyloading.js
+++ js/vanilla-lazyloading.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla Lazy Loading
- * Version: 0.5.0
+ * Version: 0.6.0
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla Fake Select may be freely distributed under the MIT license.
  */
@@ -17,6 +17,7 @@ function vanillaLazyLoading() {
     var scrollTop = 0,
         offsetTop = 200,
         imgs_vll,
+        imgs_actionchildren,
         imgs,
         timeoutImgPosition;
 
@@ -30,12 +31,37 @@ function vanillaLazyLoading() {
         scrollEvent();
         window.addEventListener('scroll', scrollEvent, 1);
         window.addEventListener('resize', timeoutImagesPosition, 1);
+        for (var i = 0, len = imgs_actionchildren.length; i < len; i++) {
+            imgs_actionchildren[i].addEventListener('mousemove', actionEvent, 1);
+            imgs_actionchildren[i].addEventListener('touchstart', actionEvent, 1);
+            imgs_actionchildren[i].addEventListener('click', actionEvent, 1);
+        }
+    }
+
+    function actionEvent() {
+        /*jshint validthis: true */
+        /* Launch only once */
+        this.removeEventListener('mousemove', actionEvent, 1);
+        this.removeEventListener('touchstart', actionEvent, 1);
+        this.removeEventListener('click', actionEvent, 1);
+        /* Load images */
+        var imgs = this.querySelectorAll('[data-vllactionsrc]'),
+            imgTmp;
+        for (var i = 0, len = imgs.length; i < len; i++) {
+            /* Set image */
+            imgTmp = {
+                el: imgs[i]
+            };
+            setImagePosition(imgTmp, 'data-vllactionsrc');
+            /* Load it */
+            loadImage(imgTmp);
+        }
     }
 
     function scrollEvent() {
         scrollTop = window.innerHeight + window.scrollY + offsetTop;
         for (var i = 0, len = imgs_vll.length; i < len; i++) {
-            loadImage(i);
+            loadItem(i);
         }
     }
 
@@ -49,6 +75,7 @@ function vanillaLazyLoading() {
             };
             imgs_vll.push(tmp_img);
         }
+        imgs_actionchildren = document.querySelectorAll('[data-vllactionchildren]');
     }
 
     function timeoutImagesPosition() {
@@ -57,35 +84,38 @@ function vanillaLazyLoading() {
     }
 
     function setImagesPosition() {
-        var tmpOffset, i, len;
-        for (i = 0, len = imgs_vll.length; i < len; i++) {
+        for (var i = 0, len = imgs_vll.length; i < len; i++) {
             if (!imgs_vll[i]) {
                 continue;
             }
-            tmpOffset = imgs_vll[i].el.getAttribute('data-vlloffset') ? parseInt(imgs_vll[i].el.getAttribute('data-vlloffset'), 10) : 0;
-            imgs_vll[i].type = imgs_vll[i].el.getAttribute('data-vlltype') ? imgs_vll[i].el.getAttribute('data-vlltype') : 'image';
-            imgs_vll[i].classname = imgs_vll[i].el.getAttribute('data-vllclassname') ? imgs_vll[i].el.getAttribute('data-vllclassname') : '';
-            imgs_vll[i].src = imgs_vll[i].el.getAttribute('data-vllsrc');
-            imgs_vll[i].top = imgs_vll[i].el.getBoundingClientRect().top + tmpOffset;
-            imgs_vll[i].target = imgs_vll[i].el;
-            if (imgs_vll[i].el.getAttribute('data-vlltarget')) {
-                switch (imgs_vll[i].el.getAttribute('data-vlltarget')) {
-                    case 'parent':
-                        imgs_vll[i].target = imgs_vll[i].el.parentNode;
-                        break;
-                    case 'child':
-                        if (imgs_vll[i].el.childNodes[0]) {
-                            imgs_vll[i].target = imgs_vll[i].el.childNodes[0];
-                        }
-                        break;
-                    default:
+            setImagePosition(imgs_vll[i], 'data-vllsrc');
+        }
+    }
 
-                }
+    function setImagePosition(img, attrSrc) {
+        var tmpOffset = img.el.getAttribute('data-vlloffset') ? parseInt(img.el.getAttribute('data-vlloffset'), 10) : 0;
+        img.type = img.el.getAttribute('data-vlltype') ? img.el.getAttribute('data-vlltype') : 'image';
+        img.classname = img.el.getAttribute('data-vllclassname') ? img.el.getAttribute('data-vllclassname') : '';
+        img.src = img.el.getAttribute(attrSrc);
+        img.top = img.el.getBoundingClientRect().top + tmpOffset;
+        img.target = img.el;
+        if (img.el.getAttribute('data-vlltarget')) {
+            switch (img.el.getAttribute('data-vlltarget')) {
+                case 'parent':
+                    img.target = img.el.parentNode;
+                    break;
+                case 'child':
+                    if (img.el.childNodes[0]) {
+                        img.target = img.el.childNodes[0];
+                    }
+                    break;
+                default:
+
             }
         }
     }
 
-    function loadImage(i) {
+    function loadItem(i) {
         // No image : stop script
         if (!imgs_vll[i]) {
             return false;
@@ -94,32 +124,37 @@ function vanillaLazyLoading() {
         if (imgs_vll[i].top > scrollTop) {
             return false;
         }
-        switch (imgs_vll[i].type) {
-            case 'background':
-                // Load image background
-                imgs_vll[i].target.style.backgroundImage = 'url(' + imgs_vll[i].src + ')';
-                break;
-            case 'none':
-                // To avoid conflicts with the callback
-                break;
-            case 'classname':
-                imgs_vll[i].target.className = imgs_vll[i].target.className + ' ' + imgs_vll[i].classname;
-                break;
-            default:
-                // Update images position on load
-                imgs_vll[i].target.addEventListener('load', timeoutImagesPosition, 1);
-                // Load image source
-                imgs_vll[i].target.src = imgs_vll[i].src;
-        }
 
-        // Callback
-        triggerEvent(imgs_vll[i].el, 'vllload', imgs_vll[i]);
+        loadImage(imgs_vll[i]);
 
         // Remove attribute
         imgs_vll[i].el.removeAttribute('data-vllsrc');
 
         // Invalidate image
         imgs_vll[i] = false;
+    }
+
+    function loadImage(img) {
+        switch (img.type) {
+            case 'background':
+                // Load image background
+                img.target.style.backgroundImage = 'url(' + img.src + ')';
+                break;
+            case 'none':
+                // To avoid conflicts with the callback
+                break;
+            case 'classname':
+                img.target.className = img.target.className + ' ' + img.classname;
+                break;
+            default:
+                // Update images position on load
+                img.target.addEventListener('load', timeoutImagesPosition, 1);
+                // Load image source
+                img.target.src = img.src;
+        }
+
+        // Callback
+        triggerEvent(img.el, 'vllload', img);
     }
 
     function triggerEvent(el, eventName, parameters) {
